ca034e9c50abc1b52269a834fce87710e3c1375d
Luisa Fernanda Cortezano Movilla <luisacortezano135@gmail.com>
2023-09-13 15:29:47 -0500

    cambios

--- CANCHASSINTETICAS/app/controllers/home_controller.rb
+++ CANCHASSINTETICAS/app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def dashboard
     @cantidad_canchas = Cancha.all.size
     @cantidad_clientes = Cliente.all.size
