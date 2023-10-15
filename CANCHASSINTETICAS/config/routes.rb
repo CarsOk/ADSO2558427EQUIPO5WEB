3fe77fa05dd97e2f9b32e09f4e66f152de392869
Luisa Fernanda Cortezano Movilla <luisacortezano135@gmail.com>
2023-10-14 21:40:51 -0500

    Orden de la plantilla

--- CANCHASSINTETICAS/config/routes.rb
+++ CANCHASSINTETICAS/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_scope :usuario do
     get '/usuarios/sign_out', to: 'devise/sessions#destroy'
   end
-  root to: "home#dashboard"
+  root to: "home#landing_page"
+  get 'dashboard', to: 'home#dashboard'
   resources :canchas
   resources :clientes
   resources :reservas
