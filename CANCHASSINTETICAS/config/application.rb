8d43f78a5478601e997c4ff84c4786580bca154a
Luisa Fernanda Cortezano Movilla <luisacortezano135@gmail.com>
2023-10-20 01:40:16 -0500

    Agregar estilos al devise

--- CANCHASSINTETICAS/config/application.rb
+++ CANCHASSINTETICAS/config/application.rb
@@ -19,10 +19,11 @@ module CANCHASSINTETICAS
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.to_prepare do
-      Devise::SessionsController.layout "login"
-      Devise::RegistrationsController.layout "login"
-      Devise::ConfirmationsController.layout "login"
-      Devise::UnlocksController.layout "login"            
-      Devise::PasswordsController.layout "login"
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout "devise"
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"            
+      Devise::PasswordsController.layout "devise"
+    end
   end
 end
