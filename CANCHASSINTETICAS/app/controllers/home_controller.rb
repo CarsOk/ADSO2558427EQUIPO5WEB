13731c49fe56a1712c7d770785eb30e6da97a4ad
Luisa Fernanda Cortezano Movilla <luisacortezano135@gmail.com>
2023-10-19 22:45:03 -0500

    Arreglos en el orden de la plantilla

--- CANCHASSINTETICAS/app/controllers/home_controller.rb
+++ CANCHASSINTETICAS/app/controllers/home_controller.rb
@@ -1,13 +1,10 @@
 class HomeController < ApplicationController
   layout "application", except: [:landing_page]
   before_action :authenticate_usuario!, only: [:dashboard]
-  before_action :authenticate_administrador!, only: [:admin]
 
   def dashboard
   end
 
-  def admin
-  end
   def landing_page
     render layout: false
   end
--- CANCHASSINTETICAS/config/routes.rb
+++ CANCHASSINTETICAS/config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :administrador, controllers: {
-    sessions: 'administrador/sessions',
-    registrations: 'administrador/registrations'
-  }
-  devise_scope :administrador do
-    get '/administrador/sign_out', to: 'devise/sessions#destroy'
-  end
-
   devise_for :usuarios, controllers: {
     sessions: 'usuarios/sessions',
     registrations: 'usuarios/registrations',
@@ -20,7 +12,6 @@ Rails.application.routes.draw do
   
   root to: "home#landing_page"
   get 'dashboard', to: 'home#dashboard'
-  get 'admin', to: 'home#admin'
   resources :canchas
   resources :clientes
   resources :reservas
